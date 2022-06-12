866389747bdad388e2c715041ff4ec80b20e836b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-06-12 15:31:49 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 let repeat = 0
+let num2 = 0
 let num = 0
+let numy = 0
+let numx = 0
 let dis = 0
 let test1 = 0
 let LeftPos = 0
@@ -25,9 +28,9 @@ let mySprite = sprites.create(img`
 let Detector = sprites.create(img`
     2 
     `, SpriteKind.Player)
-let FOV = 1
-controller.moveSprite(mySprite)
+let FOV = 0.5
 let render_dis = 100
+controller.moveSprite(mySprite)
 game.onUpdate(function () {
     scene.setBackgroundImage(img`
         ................................................................................................................................................................
@@ -154,14 +157,17 @@ game.onUpdate(function () {
     LeftPos += controller.player2.dx(400)
     for (let index = 0; index <= 159; index++) {
         test1 = dis
-        dis += -64
-        for (let index2 = 0; index2 < 8; index2++) {
+        dis += -32
+        for (let index2 = 0; index2 < 4; index2++) {
             if (!(tiles.tileAtLocationEquals(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16), assets.tile`transparency16`))) {
                 for (let index2 = 0; index2 <= 15; index2++) {
-                    if (!(tiles.tileAtLocationEquals(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16), assets.tile`transparency16`))) {
-                        num = Math.map(dis * Math.tan(FOV / 400) * 200, 0, render_dis * 16, 60, 0)
-                        for (let index3 = 0; index3 <= 60 + num * num / 80 - (60 - num * num / 80); index3++) {
-                            scene.backgroundImage().setPixel(index, 60 - num * num / 80 + index3, tiles.tileImageAtLocation(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16)).getPixel((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis + (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis)) % 16, index3 / ((60 + num * num / 80 - (60 - num * num / 80)) / 16)))
+                    numx = mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis
+                    numy = mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis
+                    if (!(tiles.tileAtLocationEquals(tiles.getTileLocation(numx / 16, numy / 16), assets.tile`transparency16`))) {
+                        num = Math.map(dis * 1, 0, render_dis * 8, 60, 0)
+                        num2 = (60 + num - (60 - num)) / 16
+                        for (let index3 = 0; index3 <= 60 + num - (60 - num); index3++) {
+                            scene.backgroundImage().setPixel(index, 60 - num + index3, tiles.tileImageAtLocation(tiles.getTileLocation(numx / 16, numy / 16)).getPixel(numx % 16, index3 / num2))
                         }
                         dis = test1
                         index += 1
@@ -176,10 +182,13 @@ game.onUpdate(function () {
         for (let index2 = 0; index2 < repeat; index2++) {
             if (!(tiles.tileAtLocationEquals(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16), assets.tile`transparency16`))) {
                 for (let index2 = 0; index2 <= 15; index2++) {
-                    if (!(tiles.tileAtLocationEquals(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16), assets.tile`transparency16`))) {
-                        num = Math.map(dis * Math.tan(FOV / 400) * 200, 0, render_dis * 16, 60, 0)
-                        for (let index3 = 0; index3 <= 60 + num * num / 80 - (60 - num * num / 80); index3++) {
-                            scene.backgroundImage().setPixel(index, 60 - num * num / 80 + index3, tiles.tileImageAtLocation(tiles.getTileLocation((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis) / 16, (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis) / 16)).getPixel((mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis + (mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis)) % 16, index3 / ((60 + num * num / 80 - (60 - num * num / 80)) / 16)))
+                    numx = mySprite.x + Math.cos((LeftPos + index * FOV) / 200) * dis
+                    numy = mySprite.y + Math.sin((LeftPos + index * FOV) / 200) * dis
+                    if (!(tiles.tileAtLocationEquals(tiles.getTileLocation(numx / 16, numy / 16), assets.tile`transparency16`))) {
+                        num = Math.map(dis * 1, 0, render_dis * 8, 60, 0)
+                        num2 = (60 + num - (60 - num)) / 16
+                        for (let index3 = 0; index3 <= 60 + num - (60 - num); index3++) {
+                            scene.backgroundImage().setPixel(index, 60 - num + index3, tiles.tileImageAtLocation(tiles.getTileLocation(numx / 16, numy / 16)).getPixel(numx % 16, index3 / num2))
                         }
                         repeat = 0
                         break;
